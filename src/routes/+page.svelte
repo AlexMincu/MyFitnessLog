a97b8a943922ad5681208db4711d3a07433cafa5
Alex <minculescualex98@gmail.com>
2023-06-17 21:05:50 +0300

    hero page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,53 @@
-<div class="container mx-auto flex h-full flex-col items-center justify-center text-center">
-	<h1 class="h1 my-5">Welcome to MyFitnessLog</h1>
+<div
+	class="container mx-auto flex h-full flex-col gap-32 items-center justify-start my-10 text-center px-6"
+>
+	<div class="flex flex-col items-center">
+		<h1 class="h1 text-5xl font-semibold">Unlock your potential with MyFitnessLog!</h1>
 
-	<h2 class="h2">Become a member</h2>
+		<h2 class="h2 my-6">Become a member</h2>
 
-	<a class="btn variant-filled-primary my-5 ml-2 h-12 w-24 rounded-2xl" href="/register">Sign up</a>
+		<a class="btn variant-filled-primary h-12 w-24 rounded-2xl" href="/register">Sign up</a>
+	</div>
+
+	<div class="flex flex-col items-center">
+		<h1 class="h1 text-5xl font-semibold">Log Workouts with Ease</h1>
+
+		<h2 class="h3 my-6">
+			Stay accountable and maintain a comprehensive log of your fitness activities.
+		</h2>
+	</div>
+
+	<div class="flex flex-col items-center">
+		<h1 class="h1 text-5xl font-semibold">Customize Your Sets</h1>
+
+		<h2 class="h3 my-6">
+			Take your workouts to the next level and make the most out of your training sessions.
+		</h2>
+	</div>
+
+	<div class="flex flex-col items-center">
+		<h1 class="h1 text-5xl font-semibold">Review Your Exercise History</h1>
+
+		<h2 class="h3 my-6">Track your exercise history and analyze your progress over time.</h2>
+	</div>
+
+	<div class="flex flex-col items-center">
+		<h1 class="h1 text-5xl font-semibold">Exercise Database</h1>
+
+		<h2 class="h3 my-6">
+			A wide range of exercises for all fitness levels. Add your custom exercises
+		</h2>
+	</div>
+
+	<div class="flex flex-col items-center">
+		<h1 class="h1 text-5xl font-semibold">Light and Dark Modes</h1>
+
+		<h2 class="h3 my-6">
+			Enjoy the flexibility of choosing between a light and dark mode interface
+		</h2>
+	</div>
+
+	<div class="flex flex-col items-center">
+		<h1 class="h1 text-5xl font-semibold">Stay Tuned for More</h1>
+	</div>
 </div>
